a035a6222fe628b7eb872634fc80f0385abb7437
freak07 <michalechner92@googlemail.com>
2017-10-12 09:35:53 +0200

    Revert "sched: Use schedtune boosted WALT signal for RT"
    This reverts commit 807a50d14ff25f8bd760b61a08610d8bc44dd801.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -95,14 +95,6 @@
 #include <trace/events/sched.h>
 #include "walt.h"
 
-void start_bandwidth_timer(struct hrtimer *period_timer, ktime_t period)
-{
-	if (hrtimer_active(period_timer))
-		return;
-
-	hrtimer_forward_now(period_timer, period);
-	hrtimer_start_expires(period_timer, HRTIMER_MODE_ABS_PINNED);
-}
 
 void start_bandwidth_timer(struct hrtimer *period_timer, ktime_t period)
 {
@@ -2986,7 +2978,7 @@ unsigned long sum_capacity_reqs(unsigned long cfs_cap,
 unsigned long boosted_cpu_util(int cpu);
 static void sched_freq_tick_pelt(int cpu)
 {
-	unsigned long cpu_utilization = boosted_cpu_util(cpu);
+	unsigned long cpu_utilization = cpu_util(cpu, UTIL_EST);
 	unsigned long capacity_curr = capacity_curr_of(cpu);
 	struct sched_capacity_reqs *scr;
 
@@ -3008,12 +3000,19 @@ static void sched_freq_tick_pelt(int cpu)
 #ifdef CONFIG_SCHED_WALT
 static void sched_freq_tick_walt(int cpu)
 {
-	unsigned long cpu_utilization = boosted_cpu_util(cpu);
+	unsigned long cpu_utilization = cpu_util(cpu, UTIL_EST);
 	unsigned long capacity_curr = capacity_curr_of(cpu);
 
 	if (walt_disabled || !sysctl_sched_use_walt_cpu_util)
 		return sched_freq_tick_pelt(cpu);
 
+	/*
+	 * Add a margin to the WALT utilization.
+	 * NOTE: WALT tracks a single CPU signal for all the scheduling
+	 * classes, thus this margin is going to be added to the DL class as
+	 * well, which is something we do not do in sched_freq_tick_pelt case.
+	 */
+	cpu_utilization = add_capacity_margin(cpu_utilization);
 	if (cpu_utilization <= capacity_curr)
 		return;
 

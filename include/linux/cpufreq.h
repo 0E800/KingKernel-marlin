b16306b253e7942ebb0bd18a89b4c533d67c23f3
AndiP <andreasp@gmx.de>
2018-08-22 20:13:44 +0100

    cpufreq: Add impulse governor!

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -546,12 +546,12 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
-<<<<<<< HEAD
-=======
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
->>>>>>> f69e97022aa... Add ConservativeX Governor and remove bad edits
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
